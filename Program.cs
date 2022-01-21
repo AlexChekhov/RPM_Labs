222224ce8212d14701b9743b5a5960c204bdfb88
DemonEyes12 <anemnonov2@gmail.com>
2022-01-21 11:08:12 +0300

    Первая лабораторная

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using MyLibrary;
+
+namespace labs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MallarDuck mallarDuck = new MallarDuck();
+            RedheadDuck redheadDuck = new RedheadDuck();
+
+            Duck[] ducks =
+            {
+                mallarDuck, redheadDuck
+            };
+
+            for (int i = 0; i < ducks.Length; i++)
+            {
+                Console.WriteLine(ducks[i].Display());
+                Console.WriteLine(ducks[i].Swim());
+                Console.WriteLine(ducks[i].Quack());
+            }
+        }
+    }
+}
